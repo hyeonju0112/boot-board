2fbe5ffb53ff381c6c29f8d2b8568a201520a8b5
user <aa@.naver.com>
2019-05-08 17:09:49 +0900

    커밋(작업 완료 표시) -staged Changes에 목록이 존재할 때

---
+++ src/main/java/hello/Greet.java
@@ -0,0 +1,8 @@
+package hello;
+
+public interface Greet {
+	
+	public String sayHello(); //선언부만 존재함
+
+
+}
---
+++ src/main/java/hello/Greeter2.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class Greeter2 implements Greet {
+
+    public String sayHello() {
+    	return "hello";
+    }
+    public String iam2() { //추가한 커스템 메소드
+        return "english people";
+    }
+
+}
